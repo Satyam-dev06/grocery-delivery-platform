821e33b5c36c38401158786a8271e6f7be034ece
SATYAM <satyamdev0611@gmail.com>
2026-07-15 03:54:30 +0530

    Enhance homepage UI with offers and trending products section

--- client/js/products.js
+++ client/js/products.js
@@ -1,47 +1,51 @@
 const products = [
 
-    {
+{
     id:1,
     name:"Fresh Milk",
     price:65,
+    oldPrice:85,
+    discount:"24%",
     image:"./images/milk.png",
     category:"Dairy",
     rating:5,
-    offer:"20% OFF",
-    stock:15
+    stock:"In Stock"
 },
 
-    {
+{
     id:2,
     name:"Red Apple",
     price:120,
+    oldPrice:150,
+    discount:"20%",
     image:"./images/apple.png",
     category:"Fruits",
     rating:5,
-    offer:"10% OFF",
-    stock:8
+    stock:"In Stock"
 },
 
-    {
+{
     id:3,
     name:"Brown Bread",
     price:45,
+    oldPrice:60,
+    discount:"25%",
     image:"./images/bread.png",
     category:"Bakery",
     rating:4,
-    offer:"15% OFF",
-    stock:5
+    stock:"Only 5 Left"
 },
 
-    {
+{
     id:4,
     name:"Spinach",
     price:40,
+    oldPrice:55,
+    discount:"27%",
     image:"./images/spinach.png",
     category:"Vegetables",
     rating:5,
-    offer:"5% OFF",
-    stock:20
+    stock:"In Stock"
 }
 
 ];
