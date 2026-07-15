9df8301c528ff3413a51e6cf79c1b5473d588cf9
SATYAM <satyamdev0611@gmail.com>
2026-07-15 22:53:10 +0530

    Setup Express server and connect MongoDB Atlas

--- server/app.js
+++ server/app.js
@@ -1,25 +1,17 @@
 const express = require("express");
-
 const cors = require("cors");
-
-require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
 
-    res.send("🚀 Grocery Delivery Backend Running");
-
-});
-
-const PORT = 5000;
-
-app.listen(PORT, function(){
-
-    console.log(`Server Running on Port ${PORT}`);
+    res.json({
+        message: "GroceryHub API Running"
+    });
 
 });
+
+module.exports = app;
