374f928726951172b779d5a22c39c8175ef12010
SATYAM <satyamdev0611@gmail.com>
2026-07-18 22:21:02 +0530

    Complete Product CRUD module

--- server/controllers/productcontroller.js
+++ server/controllers/productcontroller.js
@@ -80,9 +80,32 @@ const deleteProduct = async (req, res) => {
 
     }
 };
+// GET /api/products/:id
+const getProductById = async (req, res) => {
+    try {
+
+        const product = await Product.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({
+                message: "Product not found"
+            });
+        }
+
+        res.status(200).json(product);
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message
+        });
+
+    }
+};
 
 module.exports = {
     getProducts,
+    getProductById,
     createProduct,
     updateProduct,
     deleteProduct
