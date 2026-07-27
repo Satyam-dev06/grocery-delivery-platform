f2e7f7102efa127ab3083441e2edf99b42760767
SATYAM <satyamdev0611@gmail.com>
2026-07-28 01:16:50 +0530

    Use production backend API

--- client/js/api.js
+++ client/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://grocery-delivery-platform-5o0b.onrender.com/api";
 
 function getToken() {
   return localStorage.getItem("token");
